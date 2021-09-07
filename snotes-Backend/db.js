f7f71ad3265be2e232d5886a3128d8bfbcac7b1d
Sarvesh <sarveshsataye36@gmail.com>
2021-09-07 15:22:55 +0530

    on 2nd commit validation are created for auth.js file and database snotes is created in that email and password validation created

--- snotes-Backend/db.js
+++ snotes-Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoUri = "mongodb://localhost:27017/snotes?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 const connectToMongo = ()=>{
 
--- snotes-Backend/routes/auth.js
+++ snotes-Backend/routes/auth.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const routers = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const { check } = require('express-validator');
 // create a user using post request :api/auth .It doesnot required authntiction
 
-routers.get('/',(req, res)=>{
+routers.post('/',[
+    body('userEmail').isEmail(),
+    body('userName','username atleast contain two caracter').isLength({ min: 2 }),
+    check('userPassword', 'The password must be 5+ chars long and contain a number')
+    .not()
+    .isIn(['123', 'userPassword', 'god'])
+    .withMessage('Do not use a common word as the password')
+    .isLength({ min: 5 })
+    .matches(/\d/)
+],(req, res)=>{
 
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        userName: req.body.userName,
+        userPassword: req.body.userPassword,
+        userEmail: req.body.userEmail,
+      }).then(user => res.json(user))
+      .catch(err=>{console.log(err),
+      res.json({errors :'Please enter unique details'})});
+    
+
     res.send(req.body);
 })
 
